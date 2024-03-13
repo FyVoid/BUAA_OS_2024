5b29d76634d2225be71c953c1fb5ef0d98ccf256
符悦 <21231042@buaa.edu.cn>
2024-03-13 20:53:13 +0800

    cmd

--- handle-ps.sh
+++ handle-ps.sh
@@ -40,12 +40,15 @@ elif [ ! -z "$CMD" ]; then
     cat $FILE | grep $CMD
 elif [ ! -z $PID ]; then
     # Your code here. (3/3)
-    out=$(cat $FILE | awk -v input=$PID '$PID==$3 {print $3}')
+    out=$(cat $FILE | awk -v input=$PID 'input==$2 {print $3}')
     echo $out
-    while [ out != "" ]
-	    do
-		    out=$(cat $FILE | awk -v input=$out '$out==$3 {print $3}')
-		    echo $out
+    while [ ! -z $out ]
+ 	    do
+		    out=$(cat $FILE | awk -v input=$out 'input==$2 {print $3}')
+		    if [ ! -z $out ];
+			    then
+		    echo $out;
+		    fi
 		done
 else
     usage
