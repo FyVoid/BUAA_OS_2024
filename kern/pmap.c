a6dc68cc43e31fca441673ee185a525cdf397956
FyVoid <mythstudiofyvoid@outlook.com>
2024-03-22 09:10:19 +0800

    exercise 1.1

--- kern/pmap.c
+++ kern/pmap.c
@@ -26,6 +26,7 @@ void mips_detect_memory(u_int _memsize) {
 
 	/* Step 2: Calculate the corresponding 'npage' value. */
 	/* Exercise 2.1: Your code here. */
+	npage = memsize / PAGE_SIZE;	// PAGE_SIZE can be found in mmu.h
 
 	printk("Memory size: %lu KiB, number of pages: %lu\n", memsize / 1024, npage);
 }
