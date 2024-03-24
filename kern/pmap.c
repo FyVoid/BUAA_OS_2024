cb6cc207e6d9a8a5dcdc67345a0ae484cc66cea3
FyVoid <mythstudiofyvoid@outlook.com>
2024-03-24 11:00:10 +0800

    try fix pgdir_walk

--- kern/pmap.c
+++ kern/pmap.c
@@ -203,7 +203,7 @@ static int pgdir_walk(Pde *pgdir, u_long va, int create, Pte **ppte) {
 
 	/* Step 3: Assign the kernel virtual address of the page table entry to '*ppte'. */
 	/* Exercise 2.6: Your code here. (3/3) */
-	*ppte = KADDR(*pgdir_entryp & (~(0xffful)) + PTX(va));
+	*ppte = KADDR((*pgdir_entryp & (~(0xffful))) + PTX(va));
 
 	return 0;
 }
