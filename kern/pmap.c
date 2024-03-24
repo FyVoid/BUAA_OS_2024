79237505f8d79e2db46623e487c23621995f0833
FyVoid <mythstudiofyvoid@outlook.com>
2024-03-24 13:00:54 +0800

    try

--- kern/pmap.c
+++ kern/pmap.c
@@ -181,7 +181,7 @@ static int pgdir_walk(Pde *pgdir, u_long va, int create, Pte **ppte) {
 
 	/* Step 1: Get the corresponding page directory entry. */
 	/* Exercise 2.6: Your code here. (1/3) */
-	pgdir_entryp = pgdir + PDX(va);
+	pgdir_entryp = PADDR(pgdir + PDX(va));
 
 	/* Step 2: If the corresponding page table is not existent (valid) then:
 	 *   * If parameter `create` is set, create one. Set the permission bits 'PTE_C_CACHEABLE |
@@ -203,7 +203,7 @@ static int pgdir_walk(Pde *pgdir, u_long va, int create, Pte **ppte) {
 
 	/* Step 3: Assign the kernel virtual address of the page table entry to '*ppte'. */
 	/* Exercise 2.6: Your code here. (3/3) */
-	*ppte = (Pte *) (*pgdir_entryp & ~(0xfff) + PTX(va));
+	*ppte = (Pte *) (KADDR(*pgdir_entryp & ~(0xfff)) + PTX(va));
 
 	return 0;
 }
