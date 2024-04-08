49a7c0d256818b116c39299344e437e28d4be1c1
FyVoid <mythstudiofyvoid@outlook.com>
2024-04-08 16:13:46 +0800

    try

--- kern/sched.c
+++ kern/sched.c
@@ -35,16 +35,16 @@ void schedule(int yield) {
 	 *   'TAILQ_FIRST', 'TAILQ_REMOVE', 'TAILQ_INSERT_TAIL'
 	 */
 	/* Exercise 3.12: Your code here. */
-	if (yield || count == 0 || e == NULL || !(e->env_status == ENV_RUNNABLE)) {
+	if (yield || count <= 0 || e == NULL || !(e->env_status == ENV_RUNNABLE)) {
 		if (e != NULL && e->env_status == ENV_RUNNABLE) {
 			TAILQ_INSERT_TAIL(&env_sched_list, e, env_sched_link);
 		}
 		panic_on(TAILQ_EMPTY(&env_sched_list));
-		curenv = TAILQ_FIRST(&env_sched_list);
-		count = curenv->env_pri;
-		TAILQ_REMOVE(&env_sched_list, curenv, env_sched_link);
+		e = TAILQ_FIRST(&env_sched_list);
+		count = e->env_pri;
+		TAILQ_REMOVE(&env_sched_list, e, env_sched_link);
 	}
 	count--;
-	env_run(curenv);
+	env_run(e);
 
 }
