e7116b51f8613b735cdabcc276df057f5924b2b2
FyVoid <mythstudiofyvoid@outlook.com>
2024-04-21 18:00:36 +0800

    4.14

--- kern/syscall_all.c
+++ kern/syscall_all.c
@@ -278,12 +278,20 @@ int sys_set_env_status(u_int envid, u_int status) {
 
 	/* Step 1: Check if 'status' is valid. */
 	/* Exercise 4.14: Your code here. (1/3) */
+	if (env->env_status != ENV_RUNNABLE && env->env_status != ENV_NOT_RUNNABLE) return -E_INVAL;
 
 	/* Step 2: Convert the envid to its corresponding 'struct Env *' using 'envid2env'. */
 	/* Exercise 4.14: Your code here. (2/3) */
+	try(envid2env(envid, &env, 1));
 
 	/* Step 3: Update 'env_sched_list' if the 'env_status' of 'env' is being changed. */
 	/* Exercise 4.14: Your code here. (3/3) */
+	if (status != ENV_RUNNABLE && env->env_status == ENV_RUNNABLE) {
+		TAILQ_REMOVE(&env_sched_list, env, env_sched_link);
+	}
+	if (status == ENV_RUNNABLE && env->env_status != ENV_RUNNABLE) {
+		TAILQ_INSERT_TAIL(&env_sched_list, env, env_sched_link);
+	}
 
 	/* Step 4: Set the 'env_status' of 'env'. */
 	env->env_status = status;
