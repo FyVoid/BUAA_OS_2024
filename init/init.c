f03a6c5ccdc229e19c181eb1e99ccf919117282d
FyVoid <mythstudiofyvoid@outlook.com>
2024-05-15 17:58:16 +0800

    try

--- init/init.c
+++ init/init.c
@@ -50,8 +50,8 @@ void mips_init(u_int argc, char **argv, char **penv, u_int ram_low_size) {
 	env_init();
 
 	// lab3:
-	// ENV_CREATE_PRIORITY(user_bare_loop, 1);
-	// ENV_CREATE_PRIORITY(user_bare_loop, 2);
+	ENV_CREATE_PRIORITY(user_bare_loop, 1);
+	ENV_CREATE_PRIORITY(user_bare_loop, 2);
 
 	// lab4:
 	ENV_CREATE(user_tltest);
@@ -67,7 +67,7 @@ void mips_init(u_int argc, char **argv, char **penv, u_int ram_low_size) {
 	ENV_CREATE(user_devtst);
 
 	// lab3:
-	// schedule(0);
+	schedule(0);
 }
 
 #endif
