10b49cb33519c2ce8c8f761eda6de58450eda7c7
FyVoid <mythstudiofyvoid@outlook.com>
2024-05-16 16:14:08 +0800

    man!

--- tests/lab5_114514/fs_check.c
+++ tests/lab5_114514/fs_check.c
@@ -5,6 +5,7 @@ int main() {
 	char buffer[256];
 	int fd_num = open("/newmotd", O_RDONLY);
 	int bytes;
+	debugf("opened\n");
 	if (fork() == 0) {
 		debugf("child\n");
 		bytes = read(fd_num, buffer, 8);
