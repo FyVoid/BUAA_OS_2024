a0e9f71d4b14a4722ab9fce90a5011113deb617c
FyVoid <mythstudiofyvoid@outlook.com>
2024-05-16 16:05:37 +0800

    man!

--- tests/lab5_114514/fs_check.c
+++ tests/lab5_114514/fs_check.c
@@ -2,15 +2,15 @@
 #include <lib.h>
 
 int main() {
-	debugf("wat");
+	fs_init();
 	char buffer[256];
 	int fd_num = open("/newmotd", O_RDWR);
 	int bytes;
-	// if (fork() == 0) {
-	// 	bytes = read(fd_num, buffer, 8);
-	// 	debugf("child buffer: %s\n", buffer);
-	// } else {
-	// 	bytes = read(fd_num, buffer, 8);
-	// 	debugf("father buffer: %s\n", buffer);
-	// }
+	if (fork() == 0) {
+		bytes = read(fd_num, buffer, 8);
+		debugf("child buffer: %s\n", buffer);
+	} else {
+		bytes = read(fd_num, buffer, 8);
+		debugf("father buffer: %s\n", buffer);
+	}
 }
